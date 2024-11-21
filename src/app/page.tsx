a52198a1d031614464bd359b67a708d2ca83fac6
zhengchchen <275781239@qq.com>
2024-11-21 21:05:43 +0800

    feat: support google oauth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
+import { auth } from "@/auth";
 import { protectServer } from "@/features/auth/utils";
 
 export default async function Home() {
-  await protectServer()
-  return (
-    <div>
-      You are logged in
-    </div>
-  );
+  await protectServer();
+
+  const session = await auth();
+
+  return <div>{JSON.stringify(session)}</div>;
 }
--- src/auth.ts
+++ src/auth.ts
@@ -1,12 +1,13 @@
 import NextAuth from "next-auth";
 import Github from "next-auth/providers/github";
+import Google from "next-auth/providers/google";
 import { DrizzleAdapter } from "@auth/drizzle-adapter";
 import { db } from "@/db/drizzle";
 
 import { users, accounts } from "@/db/schema";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  providers: [Github],
+  providers: [Github, Google],
   adapter: DrizzleAdapter(db, {
     usersTable: users,
     accountsTable: accounts,
@@ -14,5 +15,8 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   pages: {
     signIn: "/sign-in",
     error: "/sign-in",
+  },
+  session: {
+    strategy: "jwt",
   },
 });
